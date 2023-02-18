1901644dc408b5140f53ab3f8215bc916755ad0b
rayiqi <vindbaiq@gmail.com>
2023-02-18 14:23:23 +0700

    second commit

---
+++ tebakAngka.js
@@ -0,0 +1,13 @@
+let nilai = prompt("input nilai");
+
+        if(nilai >= 80 && nilai <=100){
+            alert("Nilai anda A");
+        }else if(nilai >= 60 && nilai <80){
+            alert("Nilai anda B");
+        }else if(nilai >= 40 && nilai< 50){
+            alert("Nilai anda C");
+        }else if(nilai >= 20 && nilai <40){
+            alert("Nilai anda D");
+        }else{
+            alert("Nilai anda E")
+        }
